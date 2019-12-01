4345414629514e2fab768a17de6efe56d3a679cc
Philip Daniels <Philip.Daniels1971@gmail.com>
2019-12-01 15:43:55 +0000

    Step1.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use async_std::task;
+use futures::join;
+use futures::stream::{FuturesUnordered, StreamExt};
+use std::time::{Duration, Instant};
+use std::thread;
+use rand::distributions::{Distribution, Uniform};
+
 fn main() {
-    println!("Hello, world!");
+    let start_time = Instant::now();
+
+    println!("Program finished in {} ms", start_time.elapsed().as_millis());
 }
