21053de0a15a39f67f7ee7e1d49412c634f6a6b8
Philip Daniels <Philip.Daniels1971@gmail.com>
2019-12-01 19:27:46 +0000

    Step 3.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use rand::distributions::{Distribution, Uniform};
 fn main() {
     let start_time = Instant::now();
 
-    demo_waiting_for_two_async_fns();
+    //demo_waiting_for_two_async_fns();
+    demo_waiting_for_multiple_random_sleeps();
 
     println!("Program finished in {} ms", start_time.elapsed().as_millis());
 }
@@ -40,4 +41,34 @@ async fn sleep_and_print(future_number: u32, sleep_millis: u64) {
     // thread::sleep. We must not block the thread!
     task::sleep(sleep_duration).await;
     println!("Future {} slept for {} ms on {:?}", future_number, sleep_millis, thread::current().id());
+}
+
+fn demo_waiting_for_multiple_random_sleeps() {
+    // Initialise the random number generator we will use to
+    // generate the random sleep times.
+    let between = Uniform::from(500..10_000);
+    let mut rng = rand::thread_rng();
+
+    // This special collection from the `futures` crate is what we use to
+    // hold all the futures; it is designed to efficiently poll the futures
+    // until they all complete, (in any order) which we do with a simple
+    // loop (see below).
+    let mut futures = FuturesUnordered::new();
+
+    // Create 10 futures, each of which should sleep for a random
+    // number of milliseconds. None of the futures are doing anything
+    // yet, because we are only storing them; we haven't started polling
+    // them yet.
+    for future_number in 0..10 {
+        let sleep_millis = between.sample(&mut rng);
+        futures.push(sleep_and_print(future_number, sleep_millis));
+    }
+
+    // This loop is how to wait for all the elements in a `FuturesUnordered<T>`
+    // to complete. `value_returned_from_the_future` is just the
+    // unit tuple, `()`, because we did not return anything from `sleep_and_print`.
+    task::block_on(async {
+        while let Some(_value_returned_from_the_future) = futures.next().await {
+        }
+    });
 }
